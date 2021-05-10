f597ab47f86a8208252f07ab8f4e0e7c2b50638e
Tiago880 <tiagocruz_880@hotmail.com>
2021-05-10 11:59:24 -0300

    refresh folder

--- NodeJs/FUNDAMENTOS/MODELOS/app.js
+++
@@ -1,7 +0,0 @@
-// importando o arquivo calculadora
-// e armazenando em uma constante 
-const calculadora= require("./calculadora");
-
-console.log(calculadora.sub(10,80));
-calculadora.nome = "sua calculadora";
-console.log(calculadora.nome);
